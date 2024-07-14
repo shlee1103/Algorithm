a378ab5eb08a27307e0320a48ce0fefbb917073f
LEE SONG HEE <shee981103@gmail.com>
2024-07-15 00:25:12 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,12 @@
+num_1 = int(input())
+num_2 = input()
+
+answer_3 = num_1 * int(num_2[2])
+answer_4 = num_1 * int(num_2[1])
+answer_5 = num_1 * int(num_2[0])
+answer_6 = num_1 * int(num_2)
+
+print(answer_3)
+print(answer_4)
+print(answer_5)
+print(answer_6)
